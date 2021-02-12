4bc262866bbfc0ecaaeb5f29054ff96d72831506
IchikawaKoki <k.kou.1kw5.13@icloud.com>
2021-02-12 09:14:54 +0000

    searches,tags関連のビューを作成

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,7 +2,6 @@ class Inquiry
   include ActiveModel::Model
 
   attr_accessor :name, :email, :message
-  
-  validates :email, :presence => {:message => 'メールアドレスを入力してください'}
-  validates :message, :presence => {:message => 'メールアドレスを入力してください'}
+
+  validates :message, presence: true
 end
